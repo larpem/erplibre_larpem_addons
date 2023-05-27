d1ff34d27d0ef1aa9e43c6f712d2ac4eaafdc310
Mathieu Benoit <mathben@technolibre.ca>
2023-05-26 23:55:01 -0400

    [ADD] larpem new project

---
+++ code_generator_larpem/hooks.py
@@ -0,0 +1,110 @@
+import logging
+import os
+
+from odoo import SUPERUSER_ID, _, api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+MODULE_NAME = "larpem"
+
+
+def post_init_hook(cr, e):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+
+        # The path of the actual file
+        path_module_generate = "addons/larpem_erplibre_larpem_addons"
+
+        short_name = MODULE_NAME.replace("_", " ").title()
+
+        # Add code generator
+        categ_id = env["ir.module.category"].search(
+            [("name", "=", "Uncategorized")], limit=1
+        )
+        value = {
+            "shortdesc": short_name,
+            "name": MODULE_NAME,
+            "license": "AGPL-3",
+            "category_id": categ_id.id,
+            "summary": "",
+            "author": "TechnoLibre",
+            "website": "",
+            "application": True,
+            "enable_sync_code": True,
+            "path_sync_code": path_module_generate,
+            "icon": os.path.join(
+                os.path.dirname(__file__),
+                "static",
+                "description",
+                "code_generator_icon.png",
+            ),
+        }
+
+        # TODO HUMAN: enable your functionality to generate
+        value["enable_sync_template"] = True
+        value["ignore_fields"] = ""
+        value["post_init_hook_show"] = False
+        value["uninstall_hook_show"] = False
+        value["post_init_hook_feature_code_generator"] = False
+        value["uninstall_hook_feature_code_generator"] = False
+
+        value["hook_constant_code"] = f'MODULE_NAME = "{MODULE_NAME}"'
+
+        code_generator_id = env["code.generator.module"].create(value)
+
+        # Add/Update Larpem System Point
+        model_model = "larpem.system_point"
+        model_name = "larpem_system_point"
+        dct_model = {
+            "description": "Système de pointage de LARPEM",
+        }
+        dct_field = {
+            "description": {
+                "code_generator_form_simple_view_sequence": 11,
+                "code_generator_sequence": 3,
+                "code_generator_tree_view_sequence": 11,
+                "field_description": "description",
+                "ttype": "char",
+            },
+            "explication": {
+                "code_generator_form_simple_view_sequence": 12,
+                "code_generator_sequence": 4,
+                "code_generator_tree_view_sequence": 12,
+                "field_description": "Explication",
+                "ttype": "char",
+            },
+            "name": {
+                "code_generator_form_simple_view_sequence": 10,
+                "code_generator_sequence": 2,
+                "code_generator_tree_view_sequence": 10,
+                "field_description": "Name",
+                "ttype": "char",
+            },
+            "type": {
+                "code_generator_form_simple_view_sequence": 13,
+                "code_generator_sequence": 5,
+                "code_generator_tree_view_sequence": 13,
+                "field_description": "Type",
+                "ttype": "char",
+            },
+        }
+        model_larpem_system_point = code_generator_id.add_update_model(
+            model_model,
+            model_name,
+            dct_field=dct_field,
+            dct_model=dct_model,
+        )
+
+        # Generate module
+        value = {"code_generator_ids": code_generator_id.ids}
+        env["code.generator.writer"].create(value)
+
+
+def uninstall_hook(cr, e):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        code_generator_id = env["code.generator.module"].search(
+            [("name", "=", MODULE_NAME)]
+        )
+        if code_generator_id:
+            code_generator_id.unlink()
