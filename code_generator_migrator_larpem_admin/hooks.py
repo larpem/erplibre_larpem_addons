3699a7d9768b23065b887a0f9818437dc94c7dab
Mathieu Benoit <mathben@technolibre.ca>
2023-05-26 21:49:25 -0400

    [ADD] code_generator_migrator_larpem_admin: init module to export csv rules

---
+++ code_generator_migrator_larpem_admin/hooks.py
@@ -0,0 +1,91 @@
+import logging
+import os
+import time
+
+from odoo import SUPERUSER_ID, _, api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+MODULE_NAME = "larpem"
+
+
+def post_init_hook(cr, e):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+
+        # The path of the actual file
+        path_module_generate = os.path.normpath(
+            os.path.join(os.path.dirname(__file__), "..")
+        )
+
+        short_name = MODULE_NAME.replace("_", " ").title()
+
+        # Add code generator
+        categ_id = env["ir.module.category"].search(
+            [("name", "=", "Uncategorized")]
+        )
+        value = {
+            "shortdesc": short_name,
+            "name": MODULE_NAME,
+            "license": "AGPL-3",
+            "category_id": categ_id.id,
+            "summary": "",
+            "author": "TechnoLibre",
+            "website": "",
+            "application": True,
+            "enable_sync_code": True,
+            "path_sync_code": path_module_generate,
+            # "icon": os.path.join(
+            #     os.path.basename(os.path.dirname(os.path.dirname(__file__))),
+            #     "static",
+            #     "description",
+            #     "code_generator_icon.png",
+            # ),
+        }
+
+        # TODO HUMAN: enable your functionality to generate
+        value["enable_sync_template"] = False
+        value["ignore_fields"] = ""
+        value["post_init_hook_show"] = False
+        value["uninstall_hook_show"] = False
+        value["post_init_hook_feature_code_generator"] = False
+        value["uninstall_hook_feature_code_generator"] = False
+
+        value["hook_constant_code"] = f'MODULE_NAME = "{MODULE_NAME}"'
+
+        code_generator_id = env["code.generator.module"].create(value)
+
+        # Modification of field before migration
+
+        before_time = time.process_time()
+
+        after_time = time.process_time()
+        _logger.info(
+            "DEBUG time execution hook update model db before generate_module"
+            f" {after_time - before_time}"
+        )
+
+        # Generate view
+        # Action generate view
+        wizard_view = env["code.generator.generate.views.wizard"].create(
+            {
+                "code_generator_id": code_generator_id.id,
+                "enable_generate_all": True,
+            }
+        )
+
+        wizard_view.button_generate_views()
+
+        # Generate module
+        value = {"code_generator_ids": code_generator_id.ids}
+        env["code.generator.writer"].create(value)
+
+
+def uninstall_hook(cr, e):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        code_generator_id = env["code.generator.module"].search(
+            [("name", "=", MODULE_NAME)]
+        )
+        if code_generator_id:
+            code_generator_id.unlink()
